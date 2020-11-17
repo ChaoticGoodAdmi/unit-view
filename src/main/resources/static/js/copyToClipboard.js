8e4cf7af8506a2d09c8e3e8c2a1d3a96061de156
ChaoticGoodAdmi <imonare@gmail.com>
2020-11-17 11:54:26 +0300

    added clipboard copying to exploding page

--- src/main/resources/static/js/copyToClipboard.js
+++ src/main/resources/static/js/copyToClipboard.js
@@ -15,3 +15,38 @@ function showCopiedMsg() {
         document.getElementById("copied-msg").innerHTML = "";
     }, 2000);
 }
+
+$(document).ready(function () {
+    $('.clickable-row').each(function () {
+        let button = $(this).find('td').children().get(0);
+        button.addEventListener('click', function (ev) {
+            ev.stopPropagation();
+        }, true);
+    });
+});
+
+function copyTableRow(buttonId) {
+    let button = document.getElementById(buttonId);
+    button.disabled = true;
+    let indexOfDelimiter = buttonId.indexOf('_');
+    let id = buttonId.substr(indexOfDelimiter + 1, buttonId.length - indexOfDelimiter);
+    let articleRow = document.getElementById('subArt_' + id);
+    const textArea = document.createElement("textarea");
+    textArea.value = articleRow.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+    replaceWithMessage(articleRow, button);
+}
+
+function replaceWithMessage(articleRow, button) {
+    const original = articleRow.innerText;
+    articleRow.innerText = 'cкопировано';
+    articleRow.classList.add('font-italic');
+    setTimeout(() => {
+        articleRow.innerHTML = original;
+        articleRow.classList.remove('font-italic');
+        button.disabled = false;
+    }, 500);
+}
